d76e3a11119a99ce608b6fb7161461210b6876f3
Xasiyy <xasidid2441@gmail.com>
2024-11-20 22:25:01 +0100

    ajout fonctions principales

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,39 @@
 /*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 10:44:56 by asdiallo          #+#    #+#             */
-/*   Updated: 2024/11/20 10:44:57 by asdiallo         ###   ########.fr       */
+/*   Updated: 2024/11/20 22:13:48 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+t_stack *stack_initial(int capacity)
+{
+	t_stack *stack;
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return (-1);
+	stack -> data = malloc(sizeof(int) * capacity);
+	if (!stack->data)
+	{
+		free(stack);
+		return (-1);
+	}
+	stack -> top = -1;
+	stack -> capacity = capacity;
+	return (stack);
+}
+
+void push(t_stack *stack, int value)
+{
+	if (stack -> top == stack -> capacity -1)
+		return ;
+	stack -> data[++stack -> top] = value;
+}
+
+t_stack *push_swap(t_stack *stack)
+{
+	if (malloc_stack(stack) < 0 || stack -> top == -1)
+		return (NULL);
+	
+}
--- sort_stack.c
+++ sort_stack.c
@@ -1,25 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   sort_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 10:45:41 by asdiallo          #+#    #+#             */
-/*   Updated: 2024/11/20 21:37:14 by asdiallo         ###   ########.fr       */
+/*   Created: 2024/11/20 22:18:20 by asdiallo          #+#    #+#             */
+/*   Updated: 2024/11/20 22:23:45 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+int pop(t_stack *stack)
+{
+    if(stack -> top == -1)
+        return (-1);
+	return (stack -> data[stack -> top--]);
+}
 
-typedef struct s_stack{
-	int *data;
-	int top;
-	int capacity;
-}	t_stack;
+int peek(t_stack *stack)
+{
+    if(stack -> top == -1)
+		return (-1);
+	return (stack -> data[stack -> top]);
+}
 
-#endif
+int is_empty(t_stack *stack)
+{
+	return (stack -> top == -1);
+}
