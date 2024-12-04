c8a548ee3a22398d97e827bf34557e30bdf2e37b
Xasiyy <xasidid2441@gmail.com>
2024-12-04 12:13:27 +0100

    add swap

--- srcs/swap.c
+++ srcs/swap.c
@@ -3,12 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asdiallo <asiya040906@gmailc.com>          +#+  +:+       +#+        */
+/*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 17:02:23 by asdiallo          #+#    #+#             */
-/*   Updated: 2024/12/03 17:08:49 by asdiallo         ###   ########.fr       */
+/*   Updated: 2024/12/04 12:13:01 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	swap(t_stack *stack)
+{
+	t_node *first;
+	t_node *second;
+	
+	if (stack->top && stack->top->next && stack->top != stack->top->next)
+	{
+		first = stack->top;
+		second = stack->top->next;
+		fisrt->next = second->next;
+		second->next->prev = first;
+		second->next = first;
+		second->prev = fisrt->prev;
+		first->prev->next = second;
+		first->prev = second;
+		stack->top = second;
+	}
+}
+
+void	sa(t_stack *stack_a)
+{
+	write (1, "sa\n", 3);
+	swap(stack_a);
+}
+
+void	sb(t_stack *stack_b)
+{
+	write (1, "sb\n", 3);
+	swap (stack_b);
+}
+
+void	ss(t_stack *stack_a, t_stack *stack_b)
+{
+	write (1, "ss\n", 3);
+	swap (stack_a);
+	swap (stack_b);
+}
