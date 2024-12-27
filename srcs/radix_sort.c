cbfb957cec6de1bee67e235cd97bd11fc5ad6afe
Asiya Diallo <asdiallo@k0r1p16.42mulhouse.fr>
2024-12-27 10:59:10 +0100

    tourne en rond comme dhab

--- srcs/radix_sort.c
+++ srcs/radix_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   radix_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
+/*   By: asdiallo <asiya040906@gmailc.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:24:11 by asdiallo          #+#    #+#             */
-/*   Updated: 2024/12/23 15:58:24 by asdiallo         ###   ########.fr       */
+/*   Updated: 2024/12/27 10:57:30 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,61 +27,71 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 	num_bits = 0;
 	while ((max_value >> num_bits) != 0)
 		num_bits++;
-	i = 0;
-	while (i < num_bits)
+	while(!is_sorted(stack_a))
 	{
-		size = stack_size(stack_a);
-		j = 0;
-		while (j < size)
+		i = 0;
+		while (i < num_bits)
 		{
-			if (((stack_a->top->value >> i) & 1) == 1)
-				ra(stack_a);
-			else
-				pb(stack_a, stack_b);
-			j++;
+			size = stack_size(stack_a);
+			j = 0;
+			while (j < size)
+			{
+				if (((stack_a->top->value >> i) & 1) == 1)
+					ra(stack_a);
+				else
+					pb(stack_a, stack_b);
+				j++;
+			}
+			while (stack_size(stack_b) > 1 && stack_b->top->value < stack_b->top->next->value)
+				sb(stack_b);
+			while (stack_size(stack_b) > 0)
+				pa(stack_b, stack_a);
+			i++;
 		}
-		while (stack_size(stack_b) > 1 && stack_b->top->value < stack_b->top->next->value)
-			sb(stack_b);
-		i++;
-	}
-	while (stack_size(stack_b) > 0)
-		pa(stack_b, stack_a);
-	rotations = 0;
-	min_value = find_min(stack_a);
-	current = stack_a->top;
-	while (current)
-	{
-		if (current->value == min_value)
-			break;
-		rotations++;
-		current = current->next;
-	}
-	if (rotations <= stack_size(stack_a) / 2)
-	{
-		while (rotations-- > 0)
-			ra(stack_a);
-	}
-	else
-	{
-		rotations = stack_size(stack_a) - rotations;	
-		while (rotations-- > 0)
-			rra(stack_a);
+		rotations = 0;
+		min_value = find_min(stack_a);
+		current = stack_a->top;
+		while (current)
+		{
+			if (current->value == min_value)
+				break;
+			rotations++;
+			current = current->next;
+		}
+		if (rotations <= stack_size(stack_a) / 2)
+		{
+			while (rotations-- > 0)
+				ra(stack_a);
+		}
+		else
+		{
+			rotations = stack_size(stack_a) - rotations;	
+			while (rotations-- > 0)
+				rra(stack_a);
+		}
 	}
 }
 
-/* void radix_sort(t_stack *stack_a, t_stack *stack_b)
+#include "push_swap.h"
+
+/* void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
-    int max_value;
+    int	max_value;
     int num_bits;
     int size;
     int i;
     int j;
+	int rotations;
+	int min_value;
+	t_node *current;
+
+    if (is_sorted(stack_a))
+        return;
 
     max_value = find_max(stack_a);
     num_bits = 0;
     while ((max_value >> num_bits) != 0)
         num_bits++;
-
     i = 0;
     while (i < num_bits)
     {
@@ -96,7 +106,28 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
             j++;
         }
         while (stack_size(stack_b) > 0)
-            pa(stack_a, stack_b);
+            pa(stack_b, stack_a);
         i++;
     }
+	rotations = 0;
+	min_value = find_min(stack_a);
+	current = stack_a->top;
+	while (current)
+	{
+		if(current->value == min_value)
+			break;
+		rotations++;
+		current = current->next;
+	}
+	if(rotations <= stack_size(stack_a) / 2)
+	{
+		while (rotations-- > 0)
+			ra(stack_a);
+	}
+	else
+	{
+		rotations = stack_size(stack_a) - rotations;
+		while (rotations-- > 0)
+			rra(stack_a);
+	}
 } */
