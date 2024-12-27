64934afeb9ddb1158a1db74ee8c10dd751b21e99
Asiya Diallo <asdiallo@k0r1p16.42mulhouse.fr>
2024-12-27 12:44:22 +0100

    ras

--- srcs/radix_sort.c
+++ srcs/radix_sort.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   radix_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asdiallo <asiya040906@gmail.com>           +#+  +:+       +#+        */
+/*   By: asdiallo <asiya040906@gmailc.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:24:11 by asdiallo          #+#    #+#             */
-/*   Updated: 2024/12/27 11:32:57 by asdiallo         ###   ########.fr       */
+/*   Updated: 2024/12/27 12:16:39 by asdiallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	radix_sort(t_stack *stack_a, t_stack *stack_b)
+//1080 appels pour 100
+/* void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	int	max_value;
 	int num_bits;
@@ -55,7 +56,7 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 				break;
 			rotations++;
 			current = current->next;
-		}
+		}_
 		if (rotations <= stack_size(stack_a) / 2)
 		{
 			while (rotations-- > 0)
@@ -68,11 +69,9 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 				rra(stack_a);
 		}
 	}
-}
-
-#include "push_swap.h"
-
-/* void	radix_sort(t_stack *stack_a, t_stack *stack_b)
+} */
+//1340 appels pour 100
+void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
     int	max_value;
     int num_bits;
@@ -98,13 +97,27 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
         while (j < size)
         {
             if (((stack_a->top->value >> i) & 1) == 1)
-                ra(stack_a);
+			{
+				if(stack_size(stack_b) > 1 && ((stack_b->top->value >> i) & 1) == 1)
+					rr(stack_a, stack_b);
+				else
+					ra(stack_a);
+			}
             else
                 pb(stack_a, stack_b);
             j++;
         }
         while (stack_size(stack_b) > 0)
-            pa(stack_b, stack_a);
+		{
+            if (stack_size(stack_b) > 1 && stack_b->top->value < stack_b->top->next->value)
+			{
+				if (stack_size(stack_b) > 1 && stack_b->top->value < stack_b->top->next->value)
+					ss(stack_a, stack_b);
+				else
+					sb(stack_b);
+			}
+			pa(stack_b, stack_a);
+		}
         i++;
     }
 	rotations = 0;
@@ -120,12 +133,22 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 	if(rotations <= stack_size(stack_a) / 2)
 	{
 		while (rotations-- > 0)
-			ra(stack_a);
+		{
+			if (stack_size(stack_b) > 1)
+				rr(stack_a, stack_b);
+			else
+				ra(stack_a);
+		}
 	}
 	else
 	{
 		rotations = stack_size(stack_a) - rotations;
 		while (rotations-- > 0)
-			rra(stack_a);
+		{
+			if (stack_size(stack_b) > 1)
+				rrr(stack_a, stack_b);
+			else
+				rra(stack_a);	
+		}
 	}
-} */
+}
